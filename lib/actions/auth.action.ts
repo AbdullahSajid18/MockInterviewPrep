54062171e527046a2c8274b04a5bd72f99d0ebc5
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-04-23 22:35:14 +0500

    show generated interviews on the homepage

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -10,41 +10,39 @@ export const setSessionCookie = async (idToken: string) => {
   const cookieStore = await cookies();
   const sessionCookie = await auth.createSessionCookie(idToken, {
     expiresIn: oneWeek * 1000,
-  })
+  });
 
-  cookieStore.set('session', sessionCookie, {
+  cookieStore.set("session", sessionCookie, {
     maxAge: oneWeek,
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     path: "/",
     sameSite: "lax",
-  })
+  });
 };
-
 
 // SignUp Functionality
 export const signUp = async (params: SignUpParams) => {
   const { uid, name, email } = params;
 
   try {
-    const userRecord = await db.collection('users').doc(uid).get();
+    const userRecord = await db.collection("users").doc(uid).get();
 
-    if(userRecord.exists) {
+    if (userRecord.exists) {
       return {
         success: false,
         message: "User already exists. Please sign in instead.",
       };
     }
-    await db.collection('users').doc(uid).set({
+    await db.collection("users").doc(uid).set({
       name,
-      email
+      email,
     });
 
     return {
       success: true,
       message: "Account created successfully",
-    }
-
+    };
   } catch (error: any) {
     console.error("Error signing up:", error);
 
@@ -56,16 +54,15 @@ export const signUp = async (params: SignUpParams) => {
     }
 
     return {
-        success: false,
-        message: "Error signing up",
-    }
+      success: false,
+      message: "Error signing up",
+    };
   }
 };
 
-
 // Making a Sign In functionality
 export const signIn = async (params: SignInParams) => {
-  const {email, idToken} = params;
+  const { email, idToken } = params;
 
   try {
     const userRecord = await auth.getUserByEmail(email);
@@ -73,37 +70,38 @@ export const signIn = async (params: SignInParams) => {
       return {
         success: false,
         message: "User not found. Please sign up.",
-      }
-    };
+      };
+    }
 
-    await setSessionCookie(idToken)
+    await setSessionCookie(idToken);
   } catch (error) {
     console.log(error);
     return {
       success: false,
       message: "Error signing in",
-    }
-    
+    };
   }
-
 };
 
 // Showing Home page to authenticated users
-export const getCurrentUser = async ():Promise<User |null> => {
+export const getCurrentUser = async (): Promise<User | null> => {
   const cookieStore = await cookies();
-  const sessionCookie = cookieStore.get('session')?.value;
+  const sessionCookie = cookieStore.get("session")?.value;
   if (!sessionCookie) return null;
 
   try {
     const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
-    const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
+    const userRecord = await db
+      .collection("users")
+      .doc(decodedClaims.uid)
+      .get();
 
     if (!userRecord.exists) return null;
 
     return {
       ...userRecord.data(),
       id: userRecord.id,
-    }as User;
+    } as User;
   } catch (error) {
     console.log(error);
     return null;
@@ -114,4 +112,39 @@ export const getCurrentUser = async ():Promise<User |null> => {
 export const isAuthenticated = async () => {
   const user = await getCurrentUser();
   return !!user;
+};
+
+export const getInterviewsByUserId = async (
+  userId: string
+): Promise<Interview[] | null> => {
+  const interviews = await db
+    .collection("interviews")
+    .where("userId", "==", userId)
+    .orderBy("createdAt", "desc")
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+};
+
+export const getLatestInterviews = async (
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> => {
+
+  const {userId, limit = 20} = params; 
+
+  const interviews = await db
+    .collection("interviews")
+    .orderBy("createdAt", "desc")
+    .where("finalized", "==", true)
+    .where("userId", '!=', userId)
+    .limit(limit)
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
 };
