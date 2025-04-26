4bd2e5e3169badd9e1b7f873a37a2c346956fdd3
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2025-04-26 22:50:35 +0500

    finalize

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -17,7 +17,7 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
             width={38} 
             height={32} 
           />
-          <h2 className="text-primary-100">PrepWise</h2>
+          <h2 className="text-primary-100">PrepForge</h2>
         </Link>
       </nav>
 
